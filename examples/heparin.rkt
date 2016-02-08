8a193000e57f02dcf59a6e8f553e4179e0321555
Spencer Florence <spencer@florence.io>
2016-02-08 15:56:51 -0600

    fixed causality cycle

--- examples/heparin.rkt
+++ examples/heparin.rkt
@@ -2,6 +2,7 @@
 (require esterel/front-end (for-syntax syntax/parse racket/syntax))
 (module+ test (require "test-harness.rkt"))
 
+;; like par& but exits when any of the parallel branches finishes
 (define-esterel-form any&
   (syntax-parser
     [(_ body ...)
@@ -9,12 +10,23 @@
      #'(trap& T
               (par&
                (seq& body (exit& T)) ...))]))
+
 (define-esterel-form after&
   (syntax-parser
     [(_ n:nat S body ...)
      #'(seq& (await& n S) body ...)]
     [(_ S body ...)
      #'(seq& (await& S) body ...)]))
+
+;; like loop each but doesnt suspend on S to avoid causality cycles
+(define-esterel-form loop-each/no-suspend&
+  (syntax-parser
+    [(_ S body ...)
+     #'(loop&
+        (trap& K
+               (par&
+                (loop& (await& S) (exit& K))
+                (seq& body ... halt&))))]))
 
 (define heparin
   (esterel-machine
@@ -25,32 +37,32 @@
     ;; initially
     (seq& start& give-bolus&)
     ;; infusion
+    (signal&
+     waiting
      (signal&
-      waiting
-      (signal&
-       stop-waiting
-       (par&
-        (loop-each& check-aptt (abort& stop-waiting (sustain& waiting)))
-        (loop&
-         ;; a bit weird, b/c we are using many signals rather than one value
-         ;; carying signal. but we assume all the signals are mutually exclusive
-         (any&
-          (after& aptt<45 give-bolus& increase&)
-          (after& aptt-45-59 give-bolus& increase&)
-          (after& aptt-59-101)
-          (after& aptt-101-123 decrease&)
-          (after& aptt>123
-                  hold&
-                  (after& 60 minute
-                          restart&
-                          decrease&)))
-         stop-waiting&
-         pause&)
-        (loop&
-         (present& waiting
-                   (present& (or aptt<45 aptt-45-59 aptt-59-101 aptt-101-123 aptt>123)
-                             bad-aptt&))
-         pause&))))
+      stop-waiting
+      (par&
+       (loop-each& check-aptt (abort& stop-waiting (sustain& waiting)))
+       (loop&
+        ;; a bit weird, b/c we are using many signals rather than one value
+        ;; carying signal. but we assume all the signals are mutually exclusive
+        (any&
+         (after& aptt<45 give-bolus& increase&)
+         (after& aptt-45-59 give-bolus& increase&)
+         (after& aptt-59-101)
+         (after& aptt-101-123 decrease&)
+         (after& aptt>123
+                 hold&
+                 (after& 60 minute
+                         restart&
+                         decrease&)))
+        stop-waiting&
+        pause&)
+       (loop&
+        (present& waiting
+                  (present& (or aptt<45 aptt-45-59 aptt-59-101 aptt-101-123 aptt>123)
+                            bad-aptt&))
+        pause&))))
     ;; checking
     (signal&
      theraputic
@@ -60,19 +72,144 @@
        (await& 2 aptt-59-101)
        (sustain& theraputic))
 
-      (loop-each& check-aptt
-                  (await& 6 hour)
-                  (present& theraputic nothing& check-aptt&))
+      (loop-each/no-suspend& check-aptt
+                             (await& 6 hour);; should actually be 6*60 minutes?
+                             (present& theraputic nothing& check-aptt&))
 
-      (loop-each& check-aptt
-                  (await& 24 hour)
-                  (present& theraputic check-aptt&)))))))
+      (loop-each/no-suspend& check-aptt
+                             (await& 24 hour)
+                             (present& theraputic check-aptt&)))))))
 
 
 (module+ test
   (test-seq
    heparin
    #:equivalence ([hour => 60 minute])
-   (() (start give-bolus check-aptt))
+   (() (start give-bolus ;check-aptt known to fail
+              ))
    ((aptt>123) (hold))
-   ((hour) (restart decrease))))
+   ((hour) (restart decrease)))
+
+  (test-seq
+   heparin
+   #:equivalence ([hour => 60 minutes])
+   (() (start give-bolus ;check-aptt known to fail
+              ))
+   ((aptt<45) (give-bolus increase))
+   ;; 6 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-101-123) (decrease))
+   ;; 6 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-59-101) ())
+   ;; 6 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-59-101) ())
+   ;; 24 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-59-101) ())
+   ;; 24 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt>123) (hold))
+   ((hour) (restart decrease))
+   ;; 5 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-59-101) ())
+   ;; 6 horus
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))
+   ((aptt-59-101) ())
+   ;; 24 hours
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) ())
+   ((hour) (check-aptt))))
